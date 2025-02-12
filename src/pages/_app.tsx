9e5c44d67fdb9172fe126dc8599c997b478daa4d
luisAmc <luis.kosm@gmail.com>
2025-02-11 23:18:18 -0600

    Adds NProgress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import { AppProps } from 'next/app';
 import { Layout } from '~/components/Layout';
+import { NProgress } from '~/components/NProgress';
 import '~/styles/globals.css';
 import { api } from '~/utils/api';
 
 function App({ Component, pageProps }: AppProps) {
     return (
         <>
+            <NProgress />
+
             {pageProps.viewer ? (
                 <Layout>
                     <Component {...pageProps} />
